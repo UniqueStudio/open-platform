69144f3419211d2de57ed04da27fed2fad80114f
td <578279246@qq.com>
2021-01-20 22:51:19 +0800

    shorturl need test

--- db/mysql.go
+++ db/mysql.go
@@ -10,6 +10,7 @@ import (
 )
 
 var ORM *xorm.Engine
+var ORM_ShortUrl *xorm.Engine
 
 func init() {
 	var err error
@@ -26,4 +27,16 @@ func init() {
 		panic(err)
 	}
 
+	ORM_ShortUrl, err = xorm.NewEngine("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true",
+		utils.AppConfig.Mysql.User, utils.AppConfig.Mysql.Password, utils.AppConfig.Mysql.Host, utils.AppConfig.Mysql.Port, utils.AppConfig.Mysql.Database))
+
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
+	err = ORM_ShortUrl.Sync2(new(shorter))
+	if err != nil {
+		log.Println(err)
+		panic(err)
+	}
 }
